e9d755d3f32221dfc8fb714d338e9355bec28dab
singlebarrel <cpkent@gmail.com>
2019-01-15 19:31:08 -0800

    Changed target and added basic timer.

--- src/Fphi.CabinPi.Background/StartupTask.cs
+++ src/Fphi.CabinPi.Background/StartupTask.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Net.Http;
 using Windows.ApplicationModel.Background;
+using System.Diagnostics;
+using Windows.System.Threading;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
@@ -11,6 +13,11 @@ namespace Fphi.CabinPi.Background
 {
     public sealed class StartupTask : IBackgroundTask
     {
+        private BackgroundTaskDeferral _deferal;
+        private IBackgroundTaskInstance _taskInstance;
+        private ThreadPoolTimer _periodicTimer;
+        private volatile bool _cancelRequested = false;
+
         public void Run(IBackgroundTaskInstance taskInstance)
         {
             // 
@@ -20,6 +27,34 @@ namespace Fphi.CabinPi.Background
             // from closing prematurely by using BackgroundTaskDeferral as
             // described in http://aka.ms/backgroundtaskdeferral
             //
+
+            Debug.WriteLine($"{taskInstance.Task.Name} Starting");
+
+            taskInstance.Canceled += TaskInstance_Canceled;
+            _deferal = taskInstance.GetDeferral();
+            _taskInstance = taskInstance;
+
+            _periodicTimer = ThreadPoolTimer.CreatePeriodicTimer(new TimerElapsedHandler(OnTimer), TimeSpan.FromSeconds(5));
+        }
+
+        private void OnTimer(ThreadPoolTimer timer)
+        {
+            if (_cancelRequested)
+            {
+                timer.Cancel();
+            }
+            else
+            {
+                Debug.WriteLine("OnTimer");
+
+                //Here we read all of the configured sensors, add them to the in-memory store, notify the UI that we have new data, and write it out to the remote influxdb
+            }
+        }
+
+        private void TaskInstance_Canceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            _cancelRequested = true;
+            Debug.WriteLine($"{sender.Task.Name} Cancel Requested");
         }
     }
 }
