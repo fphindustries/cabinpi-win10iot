bdbd00a15f3a69d278734102bfdab3316c3ef24a
Clarke <Chris.Clarke@leg.wa.gov>
2019-01-16 19:09:17 -0800

    Added initital UI
    Just a sample of live charts formatted for the screen...actually formatted for a really tiny screen so yeah.

--- src/Fphi.CabinPi.Ui/MainPage.xaml.cs
+++ src/Fphi.CabinPi.Ui/MainPage.xaml.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Uwp;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -20,11 +25,102 @@ namespace Fphi.CabinPi.Ui
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
     {
+        private double _lastReading;
+        private double _trend;
+        private DispatcherTimer _timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(500) };
+
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            LastHourSeries = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Values = new ChartValues<ObservableValue>
+                    {
+                        new ObservableValue(3),
+                        new ObservableValue(5),
+                        new ObservableValue(6),
+                        new ObservableValue(7),
+                        new ObservableValue(3),
+                        new ObservableValue(4),
+                        new ObservableValue(2),
+                        new ObservableValue(5),
+                        new ObservableValue(8),
+                        new ObservableValue(3),
+                        new ObservableValue(5),
+                        new ObservableValue(6),
+                        new ObservableValue(7),
+                        new ObservableValue(3),
+                        new ObservableValue(4),
+                        new ObservableValue(2),
+                        new ObservableValue(5),
+                        new ObservableValue(8)
+                    }
+                }
+            };
+
+            _trend = 8;
+
+            _timer.Tick += (sender, o) =>
+            {
+                var r = new Random();
+
+                _trend += (r.NextDouble() > 0.3 ? 1 : -1) * r.Next(0, 5);
+                LastHourSeries[0].Values.Add(new ObservableValue(_trend));
+                LastHourSeries[0].Values.RemoveAt(0);
+                SetReading();
+            };
+            _timer.Start();
+
+            Vals = new ChartValues<double> { 5, 9, 8, 6, 1, 5, 7, 3, 6, 3 };
+            Nan = double.NaN;
+
+            DataContext = this;
+        }
+
+        public SeriesCollection LastHourSeries { get; set; }
+        public ChartValues<double> Vals { get; set; }
+        public double Nan { get; set; }
+        public string TodaysDate = DateTime.Now.ToString("dd MMM yyyy");
+
+        public double LastReading
+        {
+            get { return _lastReading; }
+            set
+            {
+                _lastReading = value;
+                OnPropertyChanged("LastLecture");
+            }
+        }
+
+        private async void SetReading()
+        {
+            var target = ((ChartValues<ObservableValue>)LastHourSeries[0].Values).Last().Value;
+            var step = (target - _lastReading) / 4;
+
+            await Task.Delay(100);
+            LastReading += step;
+
+            LastReading = target;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        protected void OnPropertyChanged(string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void UpdateOnclick(object sender, RoutedEventArgs e)
+        {
+            TimePowerChart.Update(true);
         }
     }
 }
+
